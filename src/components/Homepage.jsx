4c3970639f0a0d2623f15ef3bb8d949960089594
Earthpasserby <iwuno68@gmail.com>
2024-07-15 22:36:47 +0100

    header

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Container, Navbar, Nav, Col, Row } from "react-bootstrap";
+// import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Homepage = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <>
       <Container fluid>
@@ -11,7 +15,13 @@ const Homepage = () => {
             <Navbar.Brand href="#home">
               <img src="./palynx-logo.png" alt="logo" className="logo-img" />
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Toggle
+              aria-controls="responsive-navbar-nav"
+              className="border border-0 Nav-Toggle"
+              onClick={() => setExpanded(expanded ? false : "expanded")}
+            >
+              <i className={`bi ${expanded ? "bi-x" : "bi-list"}`}></i>
+            </Navbar.Toggle>
             <Navbar.Collapse id="responsive-navbar-nav" className="menu-event">
               <Nav className="me-auto">
                 <Nav.Link href="#">Home</Nav.Link>
@@ -36,7 +46,26 @@ const Homepage = () => {
           </Container>
         </Navbar>
       </Container>
-      <Container></Container>
+      <Container className="hero">
+        <Row>
+          <Col sm={8}>
+            <h2>
+              <span>Discover</span> Your City and Make New Friends with Ease
+            </h2>
+            <p>
+              Connect with people with like minds, similar interests, goals and
+              motivations and make plans all with Palynx.
+            </p>
+            <button className="btn">Download the App</button>
+          </Col>
+          <Col sm={4}>
+            <img src="./content.png" alt="palynx-img" className="img-1" />
+            <img src="./Underline.png" alt="palynx-img" className="img-3" />
+
+            <img src="./card.png" alt="palynx-img" className="img-2" />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };

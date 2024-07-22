df2244f3fdd53721712e7780ea8268c1671f084d
Earthpasserby <iwuno68@gmail.com>
2024-07-22 03:38:55 +0100

    moree homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
         },
       },

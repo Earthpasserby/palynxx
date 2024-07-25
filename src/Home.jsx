11ebf5250ab1582834e70a37faf6f2215ebd492f
Earthpasserby <iwuno68@gmail.com>
2024-07-25 01:06:55 +0100

    slides

--- src/Home.jsx
+++ src/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 2,
+    slidesToScroll: 3,
     initialSlide: 2,
     centerPadding: "130px",
     arrows: false,

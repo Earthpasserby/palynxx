0fa1aa54493206be80692d8d13ac1d1bf9171ef3
jubelo1234 <161583757+Berec1234@users.noreply.github.com>
2024-07-30 12:34:06 +0100

    seo fix

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,13 +4,13 @@ function Title({ title }) {
   return (
     <Helmet>
       <title>{title}</title>
-      <meta
+      {/* <meta
         name="description"
         content="Find the perfect companion for your next event with our app! Our app pairs you up with like-minded people who share your interests - from music festivals to sporting events. Join the waitlist!"
       />
-      <meta name="keywords" content="HTML, SEO" />
+      <meta name="keywords" content="HTML, SEO" /> */}
       <meta property="og:title" content={title} />
-      <meta property="og:type" content="website" />
+      {/* <meta property="og:type" content="website" />
       <meta
         property="og:description"
         content="Find the perfect companion for your next event with our app! Our app pairs you up with like-minded people who share your interests - from music festivals to sporting events. Join the waitlist!"
@@ -22,9 +22,9 @@ function Title({ title }) {
       <meta
         content="https://cdn.prod.website-files.com/645a668d85c9b45bb27cf73b/663377259ca1ef4de011b3f0_Palynx%20OG%20Graph.png"
         property="twitter:image"
-      ></meta>
+      />
       <meta property="og:url" content="https://www.palynx.com/" />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:card" content="summary_large_image" /> */}
     </Helmet>
   );
 }
